dc947da2755df01ba77706b4b06b148d743987ef
aishw <aishwaryashukla242002@gmail.com>
2026-03-22 00:11:20 +0530

    Home profile photo and subject semester fragment added

--- app/src/main/java/com/example/insees/MainActivity.kt
+++ app/src/main/java/com/example/insees/MainActivity.kt
@@ -7,12 +7,12 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var navController: NavController
-    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
