fc3dfe6c1b113197c2734bac5078ad318047deb5
Shreya191191 <shreyatripathi@gmail.com>
2026-03-22 13:02:37 +0530

    Fixed The AboutUs Segment

--- app/src/main/java/com/example/insees/Fragments/InseesAboutInseesFragment.kt
+++ app/src/main/java/com/example/insees/Fragments/InseesAboutInseesFragment.kt
@@ -8,19 +8,13 @@ import android.widget.GridView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.insees.Dataclasses.GridViewModal
-import com.example.insees.Utils.GridRVAdapter
+import com.example.insees.Adapters.GridRVAdapter
 import com.example.insees.R
 import com.example.insees.databinding.FragmentInseesAboutInseesBinding
-import kotlin.collections.plus
 
-/**
- * A simple [androidx.fragment.app.Fragment] subclass.
- * Use the [InseesAboutInseesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
+
 class InseesAboutInseesFragment : Fragment() {
-    lateinit var MemberGRV: GridView
-    lateinit var MemberList: List<GridViewModal>
     lateinit var binding: FragmentInseesAboutInseesBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -28,45 +22,6 @@ class InseesAboutInseesFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentInseesAboutInseesBinding.inflate(inflater, container, false)
         val view = binding.root
-
-        binding.btnBackHomeFromInsees.setOnClickListener {
-            findNavController().navigate(R.id.action_inseesAboutInseesFragment_to_inseesAboutUsFragment)
-        }
-        MemberGRV = view.findViewById(R.id.idgv)
-        MemberList = ArrayList<GridViewModal>()
-
-        // Adding data to our course list with image and course name.
-        MemberList = MemberList + GridViewModal(
-            "Name",
-            "loprem ipsum Description qwertyuiopasdfghjklzxcvbnm",
-            R.drawable.sample_img
-        )
-        MemberList = MemberList + GridViewModal(
-            "Name",
-            "loprem ipsum Description qwertyuiopasdfghjklzxcvbnm",
-            R.drawable.sample_img
-        )
-        MemberList = MemberList + GridViewModal(
-            "Name",
-            "loprem ipsum Description qwertyuiopasdfghjklzxcvbnm",
-            R.drawable.sample_img
-        )
-        MemberList = MemberList + GridViewModal(
-            "Name",
-            "loprem ipsum Description qwertyuiopasdfghjklzxcvbnm",
-            R.drawable.sample_img
-        )
-        MemberList = MemberList + GridViewModal(
-            "Name",
-            "loprem ipsum Description qwertyuiopasdfghjklzxcvbnm",
-            R.drawable.sample_img
-        )
-
-        // Initializing our course adapter and passing course list and context.
-        val courseAdapter = GridRVAdapter(MemberList = MemberList, requireContext())
-
-        // Setting adapter to our grid view.
-        MemberGRV.adapter = courseAdapter
 
         return view
 
