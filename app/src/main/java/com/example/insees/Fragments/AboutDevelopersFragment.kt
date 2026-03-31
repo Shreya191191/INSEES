9225ab2b1cf5a4e10aff88ebb971ea650063df74
Shreya191191 <shreyatripathi359@gmail.com>
2026-03-31 14:34:34 +0530

    Updates and Fixes

--- app/src/main/java/com/example/insees/Fragments/AboutDevelopersFragment.kt
+++ app/src/main/java/com/example/insees/Fragments/AboutDevelopersFragment.kt
@@ -1,19 +1,21 @@
 package com.example.insees.Fragments
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.insees.BottomSheetDialogDevelopers.AishwaryaFragment
-import com.example.insees.BottomSheetDialogDevelopers.ShreyaFragment
-
-import com.example.insees.databinding.FragmentAboutDevelopersBinding
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.widget.Toast
+import com.example.insees.BottomSheetDialogDevelopers.AnkitFragment
+import com.example.insees.BottomSheetDialogDevelopers.BishalFragment
+import com.example.insees.BottomSheetDialogDevelopers.RishiFragment
+import com.example.insees.BottomSheetDialogDevelopers.SudipFragment
+import com.example.insees.databinding.FragmentAboutDevelopersBinding
 import com.google.firebase.storage.FirebaseStorage
 import java.io.File
+
 class AboutDevelopersFragment : Fragment() {
 
     private lateinit var binding: FragmentAboutDevelopersBinding
@@ -23,34 +25,52 @@ class AboutDevelopersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentAboutDevelopersBinding.inflate(inflater,container,false)
+        binding = FragmentAboutDevelopersBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         getImages()
-        val bottomSheetAishwarya=AishwaryaFragment()
-        val bottomSheetShreya=ShreyaFragment()
 
-        binding.btnAishwarya.setOnClickListener {
-            bottomSheetAishwarya.show(childFragmentManager, "BottomSheetDialog")
+        val bottomSheetSudip = SudipFragment()
+        val bottomSheetAnkit = AnkitFragment()
+        val bottomSheetRishi = RishiFragment()
+        val bottomsheetBishal = BishalFragment()
+
+        binding.btnSudip.setOnClickListener {
+            bottomSheetSudip.show(childFragmentManager, "BottomSheetDialog")
         }
 
-        binding.btnShreya.setOnClickListener {
-            bottomSheetShreya.show(childFragmentManager, "BottomSheetDialog")
+        binding.btnAnkit.setOnClickListener {
+            bottomSheetAnkit.show(childFragmentManager, "BottomSheetDialog")
+        }
+
+        binding.btnRishi.setOnClickListener {
+            bottomSheetRishi.show(childFragmentManager, "BottomSheetDialog")
+        }
+
+        binding.btnBishal.setOnClickListener {
+            bottomsheetBishal.show(childFragmentManager, "BottomSheetDialog")
         }
     }
+
     private fun getImages() {
-        loadImage("images/aishwarya.jpg", "aishwarya.jpg") { bitmap ->
-            binding.aishwaryaImage.setImageBitmap(bitmap)
+        loadImage("images/sudip.jpg", "sudip.jpg") { bitmap ->
+            binding.sudipImage.setImageBitmap(bitmap)
         }
-        loadImage("images/shreya.jpg", "shreya.jpg") { bitmap ->
-            binding.shreyaImage.setImageBitmap(bitmap)
+        loadImage("images/ankit.jpg", "ankit.jpg") { bitmap ->
+            binding.ankitImage.setImageBitmap(bitmap)
+        }
+        loadImage("images/rishi.jpg", "rishi.jpg") { bitmap ->
+            binding.rishiImage.setImageBitmap(bitmap)
+        }
+        loadImage("images/bishal.jpg", "bishal.jpg") { bitmap ->
+            binding.bishalImage.setImageBitmap(bitmap)
         }
     }
+
     private fun loadImage(remotePath: String, localFileName: String, onImageLoaded: (bitmap: Bitmap) -> Unit) {
         val localFile = File(requireContext().cacheDir, localFileName)
 
