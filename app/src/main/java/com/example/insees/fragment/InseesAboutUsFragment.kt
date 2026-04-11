a18c3ec21866d0f80f330082c4091a7c99f582e6
aishw <aishwaryashukla242002@gmail.com>
2026-04-11 18:00:38 +0530

    Nav Drawer Fixed

--- app/src/main/java/com/example/insees/fragment/InseesAboutUsFragment.kt
+++ app/src/main/java/com/example/insees/fragment/InseesAboutUsFragment.kt
@@ -9,6 +9,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.example.insees.Fragments.AboutMembersFragment
 import com.example.insees.R
 import com.example.insees.databinding.FragmentInseesAboutUsBinding
 
